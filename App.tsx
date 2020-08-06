9c3ad7b5e30ebcdf510b97cf73ea355f156cf003
Stephen Michael <stephenmichaela@gmail.com>
2020-08-06 16:53:52 -0300

    Quarto dia NLW#02 Finalizado.

--- App.tsx
+++ App.tsx
@@ -1,17 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Landing from './src/pages/Landing';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo'
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins'
+import AppStack from './src/routes/AppStack';
 
-
+//Para carregar as fontes.
 export default function App() {
-  return (
-    //No ReactNative, não posso passar dois elementos um em baixo do outro. Precisam ter algo em volta
-    //Um alternativa é colocar uma <View> ao redor deles, porém isso carrega mesmo uma view.
-    //Uma outra alternativa é envoltar eles com <> </>. Pois não gera uma view.
-    <>
-      <Landing />
-      <StatusBar style="auto" />
-    </>
-  );
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
+
+  //Enquantos as fontes não forem carregadas, irá apresentar o Loading do Expo.
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+  else {
+    return (
+      //No ReactNative, não posso passar dois elementos um em baixo do outro. Precisam ter algo em volta
+      //Um alternativa é colocar uma <View> ao redor deles, porém isso carrega mesmo uma view.
+      //Uma outra alternativa é envoltar eles com <> </>. Pois não gera uma view.
+      <>
+        <AppStack />
+        <StatusBar style="light" />
+      </>
+    );
+  }
 }
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,19 +1,60 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native'
+//Um botão aplica o button press de acordo com o sistema operacional do usuário. 
+import { RectButton } from 'react-native-gesture-handler'; 
 
 import styles from './styles';
 
 import landingImg from '../../assets/images/landing.png';
+import studyIcon from '../../assets/images/icons/study.png';
+import giveClassesIcon from '../../assets/images/icons/give-classes.png';
+import heartIcon from '../../assets/images/icons/heart.png';
 
 function Landing() {
+    const { navigate } = useNavigation();
+
+    function handleNavigateToGiveClassesPage() {
+        navigate('GiveClasses');
+    }
+
+    function handleNavigateToStudyPages(){
+        navigate('Study');
+    }
+
     return (
         <View style={styles.container}>
             <Image source={landingImg} style={styles.banner} />
 
-            <Text styles={styles.tittle}>
+            <Text style={styles.title}>
                 Seja bem-vindo, {'\n'}
+                <Text style={styles.titleBold}>O que deseja fazer?</Text>
             </Text>
-            <Text styles={styles.titleBold}>O que deseja fazer?</Text>
+
+            <View style={styles.buttonsContainer}>
+                <RectButton 
+                onPress={handleNavigateToStudyPages}
+                style={[styles.button, styles.buttonPrimary]}>
+
+                    <Image source={studyIcon} />
+                    
+                    <Text style={styles.buttonText}>Estudar</Text>
+                </RectButton>
+
+                <RectButton
+                    onPress={handleNavigateToGiveClassesPage}
+                    style={[styles.button, styles.buttonSecondary]}
+                >
+                    <Image source={giveClassesIcon} />
+
+                    <Text style={styles.buttonText}>Dar aulas</Text>
+                </RectButton>
+            </View>
+
+            <Text style={styles.totalConnections}>
+                Total de 285 conexões já realizadas {' '}
+                <Image source={heartIcon} />
+            </Text>
         </View>
     );
 }
